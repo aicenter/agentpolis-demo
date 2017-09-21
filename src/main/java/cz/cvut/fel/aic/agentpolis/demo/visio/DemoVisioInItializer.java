ae203a62adc94b1c1da8078485652da604dcc2c8
Michal Cap <michal.cap@agents.fel.cvut.cz>
2017-09-21 21:14:31 +0200

    Visualization now uses map tiles

--- src/main/java/cz/cvut/fel/aic/agentpolis/demo/visio/DemoVisioInItializer.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/demo/visio/DemoVisioInItializer.java
@@ -12,44 +12,40 @@ import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.networks
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.networks.HighwayNetwork;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.networks.AllNetworkNodes;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.networks.PedestrianNetwork;
-import cz.cvut.fel.aic.agentpolis.simulator.visualization.visio.NodeIdLayer;
-import cz.cvut.fel.aic.agentpolis.simulator.visualization.visio.HighwayLayer;
+import cz.cvut.fel.aic.agentpolis.simulator.visualization.visio.*;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.AgentStorage;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.VehicleStorage;
 import cz.cvut.fel.aic.agentpolis.simulator.creator.SimulationCreator;
-import cz.cvut.fel.aic.agentpolis.simulator.visualization.visio.DefaultVisioInitializer;
-import cz.cvut.fel.aic.agentpolis.simulator.visualization.visio.SimulationControlLayer;
 import cz.cvut.fel.aic.alite.simulation.Simulation;
 import cz.cvut.fel.aic.alite.vis.VisManager;
 import cz.cvut.fel.aic.alite.vis.layer.VisLayer;
 import cz.cvut.fel.aic.alite.vis.layer.common.ColorLayer;
-import cz.cvut.fel.aic.agentpolis.simulator.visualization.visio.GridLayer;
+
 import java.awt.Color;
 
 @Singleton
 public class DemoVisioInItializer extends DefaultVisioInitializer {
 
     protected final NodeIdLayer nodeIdLayer;
-
     protected final HighwayLayer highwayLayer;
-
     private final VisLayer backgroundLayer;
-
     private final CarLayer carLayer;
+    private final MapTilesLayer mapTitlesLayer;
 
     @Inject
     public DemoVisioInItializer(PedestrianNetwork pedestrianNetwork, BikewayNetwork bikewayNetwork,
                                 HighwayNetwork highwayNetwork, TramwayNetwork tramwayNetwork, MetrowayNetwork metrowayNetwork,
                                 RailwayNetwork railwayNetwork, NodeIdLayer nodeIdLayer, HighwayLayer highwayLayer,
-                                SimulationControlLayer simulationControlLayer, GridLayer gridLayer, CarLayer carLayer) {
+                                SimulationControlLayer simulationControlLayer, GridLayer gridLayer, CarLayer carLayer, MapTilesLayer mapTitles) {
         super(pedestrianNetwork, bikewayNetwork, highwayNetwork, tramwayNetwork, metrowayNetwork, railwayNetwork,
                 simulationControlLayer, gridLayer);
         this.nodeIdLayer = nodeIdLayer;
         this.highwayLayer = highwayLayer;
         this.carLayer = carLayer;
         this.backgroundLayer = ColorLayer.create(Color.white);
+        this.mapTitlesLayer = mapTitles;
     }
 
     @Override
@@ -70,6 +66,7 @@ public class DemoVisioInItializer extends DefaultVisioInitializer {
     @Override
     protected void initGraphLayers() {
         VisManager.registerLayer(backgroundLayer);
+        VisManager.registerLayer(mapTitlesLayer);
         VisManager.registerLayer(highwayLayer);
     }
 
