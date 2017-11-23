bfc6136380b55e6126f53f8792bbe966f8ba2c41
Fido <david.fido.fiedler@gmail.com>
2017-11-23 18:24:42 +0100

    LGPL license header, config location files removed

--- src/main/java/cz/cvut/fel/aic/apdemo/config/ApdemoConfig.java
+++ src/main/java/cz/cvut/fel/aic/apdemo/config/ApdemoConfig.java
@@ -16,27 +16,33 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
-package cz.cvut.fel.aic.apdemo;
+package cz.cvut.fel.aic.apdemo.config;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.lang.Integer;
+import java.lang.String;
+import java.util.Map;
+import ninja.fido.config.GeneratedConfig;
 
+public class ApdemoConfig implements GeneratedConfig {
+  public String mapGeojsonEdges;
 
-import cz.cvut.fel.aic.agentpolis.simmodel.entity.EntityType;
+  public Agentpolis agentpolis;
 
-/**
- *
- * @author fido
- */
-public enum DemoType implements EntityType {
-    VEHICLE,
-    DRIVER;
+  public String mapFilePath;
 
-    @Override
-    public String getDescriptionEntityType() {
-        return "";
-    }
+  public String mapGeojsonNodes;
+
+  public Integer srid;
+
+  public ApdemoConfig() {
+  }
+
+  public ApdemoConfig fill(Map apdemoConfig) {
+    this.mapGeojsonEdges = (String) apdemoConfig.get("map_geojson_edges");
+    this.agentpolis = new Agentpolis((Map) apdemoConfig.get("agentpolis"));
+    this.mapFilePath = (String) apdemoConfig.get("map_file_path");
+    this.mapGeojsonNodes = (String) apdemoConfig.get("map_geojson_nodes");
+    this.srid = (Integer) apdemoConfig.get("srid");
+    return this;
+  }
 }
